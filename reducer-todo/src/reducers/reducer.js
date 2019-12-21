ff062ad22831c0e466f101e0bdd10a38f9c9f6a8
Jane Suh <sourcherryyogurt@gmail.com>
2019-12-20 20:01:26 -0500

    Step 1 - Build a simple reducer and initial state

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,30 @@
+export const initialState = {
+    todos: [
+        {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+        },
+        {
+            item: 'Learn about context',
+            completed: false,
+            id: 3892987590
+        },
+        {
+            item: 'Learn about testing',
+            completed: false,
+            id: 3892987591
+        },
+        {
+            item: 'Learn about class components',
+            completed: false,
+            id: 3892987592
+        }
+    ]
+}
+
+export function reducer(state, action) {
+    switch (action.type) {
+        return state;
+    }
+}
